fb6421b1c3b1ecb4959209086d48481c271a53ca
maikofelix47 <maikofelix89@gmail.com>
2025-10-17 17:59:49 +0300

    (feat) Add void dialog and operations
    (improv) changed rules to tabular view

--- apps/front-end/src/rules/rules.component.ts
+++ apps/front-end/src/rules/rules.component.ts
@@ -1,158 +1,314 @@
-import { Component, DestroyRef, inject, OnInit } from "@angular/core";
-import { FeatureFlagsService } from "../services/feature_flag.service";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { catchError, finalize, tap } from "rxjs";
-import { SnackBarUtil } from "../shared/snackbar/snackbar.util";
-import { MatDialog } from "@angular/material/dialog";
-import { AppFormDialogDefaultValues, RuleTypes } from "./rules.types";
-import { RulesService } from "../services/rules.service";
-import { OperatorsService } from "../services/operators.service";
-import { OperatorTypes } from "../operators/operators.types";
-import { CommonModule } from "@angular/common";
-import { AttributeTypes } from "../attributes/attributes.types";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { FeatureFlagsService } from '../services/feature_flag.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { catchError, EMPTY, finalize, switchMap, tap } from 'rxjs';
+import { SnackBarUtil } from '../shared/snackbar/snackbar.util';
+import { MatDialog } from '@angular/material/dialog';
+import { AppFormDialogDefaultValues, RuleTypes } from './rules.types';
+import { RulesService } from '../services/rules.service';
+import { OperatorsService } from '../services/operators.service';
+import { OperatorTypes } from '../operators/operators.types';
+import { CommonModule } from '@angular/common';
+import { AttributeTypes } from '../attributes/attributes.types';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { RulesFormComponent } from "./form/rules.form.component";
-import { AttributesService } from "../services/attributes.service";
-import { ActivatedRoute } from "@angular/router";
+import { RulesFormComponent } from './form/rules.form.component';
+import { AttributesService } from '../services/attributes.service';
+import { ActivatedRoute } from '@angular/router';
+import { TableComponent } from '../shared/table/table.component';
+import { Columns } from '../shared/table/table.component.types';
+import { MatTableDataSource } from '@angular/material/table';
+import moment from 'moment';
+import { VoidFormComponent } from '../shared/dialog/void-dialog/void.form.component';
+import { VoidDto } from '../shared/dialog/void-dialog/types';
 
 @Component({
-    imports: [CommonModule, MatFormFieldModule, MatChipsModule, MatIconModule],
-    selector: 'app-rules',
-    templateUrl: './rules.component.html',
-    styleUrl: 'rules.component.scss'
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatIconModule,
+    TableComponent,
+  ],
+  selector: 'app-rules',
+  templateUrl: './rules.component.html',
+  styleUrl: 'rules.component.scss',
 })
-
 export class RulesComponent implements OnInit {
-    readonly addOnBlur = true;
-    readonly separatorKeysCodes = [ENTER, COMMA] as const;
-    private rulesService = inject(RulesService);
-    private featureFlagsService = inject(FeatureFlagsService);
-    private operatorsService = inject(OperatorsService);
-    private attributesService = inject(AttributesService);
-    private snackBar = inject(SnackBarUtil);
-    private destroyRef = inject(DestroyRef);
-    private dialog = inject(MatDialog);
-    private route = inject(ActivatedRoute);
-    busy = false;
+  readonly addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  private rulesService = inject(RulesService);
+  private featureFlagsService = inject(FeatureFlagsService);
+  private operatorsService = inject(OperatorsService);
+  private attributesService = inject(AttributesService);
+  private snackBar = inject(SnackBarUtil);
+  private destroyRef = inject(DestroyRef);
+  private dialog = inject(MatDialog);
+  private route = inject(ActivatedRoute);
+  busy = false;
 
-    rules: RuleTypes[] = [];
-    featureFlagId = 0;
-    featureName = "";
-    featureDescription = "";
-    operators: OperatorTypes[] = [];
-    attributes: AttributeTypes[] = [];
+  rules: RuleTypes[] = [];
+  featureFlagId = 0;
+  featureName = '';
+  featureDescription = '';
+  operators: OperatorTypes[] = [];
+  attributes: AttributeTypes[] = [];
+  columns: Columns[] = [
+    {
+      name: 'No',
+      property: 'no',
+    },
+    {
+      name: 'Attribute',
+      property: 'attribute',
+    },
+    {
+      name: 'Operator',
+      property: 'operator',
+    },
+    {
+      name: 'Value',
+      property: 'value',
+    },
+    {
+      name: 'Created By',
+      property: 'createdBy',
+    },
+    {
+      name: 'Created Date',
+      property: 'createdDate',
+    },
+  ];
+  dataSource = new MatTableDataSource<any>([]);
 
-    ngOnInit() {
-        this.fetchRouteParams();
-        this.fetchRules();
-        this.fetchOperators();
-        this.fetchAttributes();
-    }
+  ngOnInit() {
+    this.fetchRouteParams();
+    this.fetchRules();
+    this.fetchOperators();
+    this.fetchAttributes();
+  }
 
-    fetchRouteParams() {
-        this.route.params
-            .pipe()
-            .forEach(param => {
-                this.featureFlagId = param["id"];
-                this.featureName = param["name"];
-                this.featureDescription = param["description"];
-            });
-    }
+  fetchRouteParams() {
+    this.route.params.pipe().forEach((param) => {
+      this.featureFlagId = param['id'];
+      this.featureName = param['name'];
+      this.featureDescription = param['description'];
+    });
+  }
 
-    fetchRules() {
-        this.busy = true;
-        this.rulesService.fetch()
-            .pipe(
-                takeUntilDestroyed(this.destroyRef),
-                tap((res) => {
-                    this.rules = res;
-                }),
-                catchError((error) => {
-                    this.snackBar.open(`An error occurred while loading operators: ${error.statusText}`, "error");
-                    throw error;
-                }),
-                finalize(() => {
-                    this.busy = false;
-                }),
-            ).subscribe();
-    }
+  generateDataSource(rules: RuleTypes[]) {
+    const ds = rules.map((rule, index) => {
+      return {
+        no: index + 1,
+        id: rule.id,
+        attribute: rule.attribute.name,
+        operator: rule.operator.name,
+        value: rule.value,
+        createdBy: rule.createdBy,
+        createdDate: moment(rule.createdDate).format('YYYY-MM-DD'),
+      };
+    });
+    this.dataSource = new MatTableDataSource(ds);
+  }
 
-    fetchOperators() {
-        this.busy = true;
-        this.operatorsService.fetch()
-            .pipe(
-                takeUntilDestroyed(this.destroyRef),
-                tap((res) => {
-                    this.operators = res;
-                }),
-                catchError((error) => {
-                    this.snackBar.open(`An error occurred while loading operators: ${error.statusText}`, "error");
-                    throw error;
-                }),
-                finalize(() => {
-                    this.busy = false;
-                }),
-            ).subscribe();
-    }
+  fetchRules() {
+    this.busy = true;
+    this.rulesService
+      .fetch()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        tap((res) => {
+          this.rules = res;
+          this.generateDataSource(this.rules);
+        }),
+        catchError((error) => {
+          this.snackBar.open(
+            `An error occurred while loading operators: ${error.statusText}`,
+            'error'
+          );
+          throw error;
+        }),
+        finalize(() => {
+          this.busy = false;
+        })
+      )
+      .subscribe();
+  }
 
-    fetchAttributes() {
-        this.busy = true;
-        this.attributesService.fetch()
-            .pipe(
-                takeUntilDestroyed(this.destroyRef),
-                tap((res) => {
-                    this.attributes = res;
-                }),
-                catchError((error) => {
-                    this.snackBar.open(`An error occurred while loading attributes: ${error.statusText}`, "error");
-                    throw error;
-                }),
-                finalize(() => {
-                    this.busy = false;
-                }),
-            ).subscribe();
-    }
+  fetchOperators() {
+    this.busy = true;
+    this.operatorsService
+      .fetch()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        tap((res) => {
+          this.operators = res;
+        }),
+        catchError((error) => {
+          this.snackBar.open(
+            `An error occurred while loading operators: ${error.statusText}`,
+            'error'
+          );
+          throw error;
+        }),
+        finalize(() => {
+          this.busy = false;
+        })
+      )
+      .subscribe();
+  }
 
-    filterOperatorRules(operatorId: number) {
-        const rules = this.rules.filter(v => v.operator.id === operatorId);
-        return rules;
-    }
+  fetchAttributes() {
+    this.busy = true;
+    this.attributesService
+      .fetch()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        tap((res) => {
+          this.attributes = res;
+        }),
+        catchError((error) => {
+          this.snackBar.open(
+            `An error occurred while loading attributes: ${error.statusText}`,
+            'error'
+          );
+          throw error;
+        }),
+        finalize(() => {
+          this.busy = false;
+        })
+      )
+      .subscribe();
+  }
 
-    add(operatorId: number): void {
-        const filteredRules = this.filterOperatorRules(operatorId);
-        const selectorAttributes = this.attributes.filter(v => !filteredRules.map(x => x.attribute.id).includes(v.id));
+  filterOperatorRules(operatorId: number) {
+    const rules = this.rules.filter((v) => v.operator.id === operatorId);
+    return rules;
+  }
 
-        this.dialog.open(RulesFormComponent, {
-            data: { ...AppFormDialogDefaultValues, attributes: selectorAttributes, operatorId, featureFlagId: this.featureFlagId },
-            width: "800px",
-            height: "auto"
-        }).afterClosed().pipe(
-            takeUntilDestroyed(this.destroyRef),
-            tap(result => {
-                if (result) {
-                    this.fetchRules();
-                }
-            })
-        ).subscribe();
-    }
+  add(operatorId: number): void {
+    const filteredRules = this.filterOperatorRules(operatorId);
+    const selectorAttributes = this.attributes.filter(
+      (v) => !filteredRules.map((x) => x.attribute.id).includes(v.id)
+    );
 
-    remove(rule: RuleTypes): void {
-        this.rulesService.delete(rule.id)
-            .pipe(
-                tap(() => {
-                    this.snackBar.open(`Rule deleted successfully`, "success");
-                }),
-                catchError((error) => {
-                    this.snackBar.open(`An error occurred while deleting the rule: ${error.statusText}`, "error");
-                    throw error;
-                }),
-                finalize(() => {
-                    this.fetchRules();
-                }),
-            )
-            .subscribe();
-    }
+    this.dialog
+      .open(RulesFormComponent, {
+        data: {
+          ...AppFormDialogDefaultValues,
+          attributes: selectorAttributes,
+          operatorId,
+          operators: this.operators,
+          featureFlagId: this.featureFlagId,
+        },
+        width: '800px',
+        height: 'auto',
+      })
+      .afterClosed()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        tap((result) => {
+          if (result) {
+            this.fetchRules();
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  remove(ruleId: number, voidDto: VoidDto) {
+    return this.rulesService.delete(ruleId, voidDto).pipe(
+      tap(() => {
+        this.snackBar.open(`Rule deleted successfully`, 'success');
+      }),
+      catchError((error) => {
+        this.snackBar.open(
+          `An error occurred while deleting the rule: ${error.statusText}`,
+          'error'
+        );
+        throw error;
+      })
+    );
+  }
+  addBtnClicked() {
+    this.dialog
+      .open(RulesFormComponent, {
+        data: {
+          ...AppFormDialogDefaultValues,
+          attributes: this.attributes,
+          operators: this.operators,
+          featureFlagId: this.featureFlagId,
+        },
+        width: '800px',
+        height: 'auto',
+      })
+      .afterClosed()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        tap((result) => {
+          if (result) {
+            this.fetchRules();
+          }
+        })
+      )
+      .subscribe();
+  }
+  editBtnClicked($event: any) {
+    const rule: RuleTypes = this.rules[$event.no - 1] ?? null;
+    this.dialog
+      .open(RulesFormComponent, {
+        data: {
+          isUpdate: true,
+          title: 'Update rule',
+          btnText: 'Update',
+          id: rule.id,
+          attributeId: rule.attribute.id,
+          operatorId: rule.operator.id,
+          value: rule.value,
+          attributes: this.attributes,
+          operators: this.operators,
+          featureFlagId: this.featureFlagId,
+        },
+        width: '800px',
+        height: 'auto',
+      })
+      .afterClosed()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        tap((result) => {
+          if (result) {
+            this.fetchRules();
+          }
+        })
+      )
+      .subscribe();
+  }
+  rulesBtnClicked($event: any) {}
+  deleteBtnClicked($event: any) {
+    this.dialog
+      .open(VoidFormComponent, {
+        data: {},
+        width: '800px',
+        height: 'auto',
+      })
+      .afterClosed()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        switchMap((res: VoidDto) => {
+          if (res) {
+            return this.remove($event.id, res);
+          } else {
+            return EMPTY;
+          }
+        }),
+        tap((result) => {
+          if (result) {
+            this.fetchRules();
+          }
+        })
+      )
+      .subscribe();
+  }
 }
--- apps/front-end/src/services/attributes.service.ts
+++ apps/front-end/src/services/attributes.service.ts
@@ -1,38 +1,43 @@
-import { Injectable } from "@angular/core";
-import { BaseService } from "./base.service";
-import { AttributeTypes } from "../attributes/attributes.types";
+import { Injectable } from '@angular/core';
+import { BaseService } from './base.service';
+import { AttributeTypes } from '../attributes/attributes.types';
+import { VoidDto } from '../shared/dialog/void-dialog/types';
 
 @Injectable({ providedIn: 'root' })
 export class AttributesService extends BaseService {
-    private endPoint = "/attributes";
+  private endPoint = '/attributes';
 
-    private getEndpoint(endPoint: string = this.endPoint) {
-        return this.getUrl("FEATURE_FLAG") + endPoint;
-    }
+  private getEndpoint(endPoint: string = this.endPoint) {
+    return this.getUrl('FEATURE_FLAG') + endPoint;
+  }
 
-    public fetch() {
-        const url = this.getEndpoint();
-        return this.http.get<AttributeTypes[]>(url);
-    }
+  public fetch() {
+    const url = this.getEndpoint();
+    return this.http.get<AttributeTypes[]>(url);
+  }
 
-    public create(name: string, description: string) {
-        const url = this.getEndpoint();
-        return this.http.post(url, {
-            name,
-            description
-        });
-    }
+  public create(name: string, description: string) {
+    const url = this.getEndpoint();
+    return this.http.post(url, {
+      name,
+      description,
+    });
+  }
 
-    public update(id: number, name: string, description: string) {
-        const url = this.getEndpoint() + "/" + id;
-        return this.http.patch(url, {
-            name,
-            description
-        });
-    }
+  public update(id: number, name: string, description: string) {
+    const url = this.getEndpoint() + '/' + id;
+    return this.http.patch(url, {
+      name,
+      description,
+    });
+  }
 
-    public delete() {
-        const url = this.getEndpoint();
-        return this.http.delete(url, {});
-    }
+  public delete(id: number, voidDto: VoidDto) {
+    const url = this.getEndpoint() + '/' + id;
+    return this.http.delete(url, {
+      body: {
+        ...voidDto,
+      },
+    });
+  }
 }
